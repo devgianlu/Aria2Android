2a969b3c1603a5aaf94b98309940c5aa815f9664
Gianlu <altomanigianluca@gmail.com>
2016-08-14 13:18:18 +0200

    Initial commit

---
+++ app/src/main/java/com/gianlu/aria2android/MainActivity.java
@@ -0,0 +1,13 @@
+package com.gianlu.aria2android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
