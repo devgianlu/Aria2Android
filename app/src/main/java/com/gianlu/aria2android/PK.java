40eac0b628582cc26a5a36f3689d2269f4dd4894
devgianlu <altomanigianluca@gmail.com>
2019-11-27 20:41:00 +0100

    Do not download binary, it is bundled inside the APK

--- app/src/main/java/com/gianlu/aria2android/PK.java
+++ app/src/main/java/com/gianlu/aria2android/PK.java
@@ -7,6 +7,5 @@ import com.gianlu.commonutils.preferences.Prefs;
 public final class PK extends Aria2PK {
     public static final Prefs.Key CURRENT_SESSION_START = new Prefs.Key("currentSessionStart");
     public static final Prefs.KeyWithDefault<Boolean> START_AT_BOOT = new Prefs.KeyWithDefault<>("startAtBoot", false);
-    static final Prefs.Key IS_NEW_V2 = new Prefs.Key("isNewV2");
     static final Prefs.Key IS_NEW_BUNDLED_WITH_ARIA2APP = new Prefs.Key("bundledWithAria2App");
 }
--- app/src/main/java/com/gianlu/aria2android/ThisApplication.java
+++ app/src/main/java/com/gianlu/aria2android/ThisApplication.java
@@ -1,13 +1,9 @@
 package com.gianlu.aria2android;
 
 
-import com.gianlu.aria2lib.Aria2PK;
 import com.gianlu.aria2lib.Aria2Ui;
 import com.gianlu.commonutils.analytics.AnalyticsApplication;
 import com.gianlu.commonutils.logging.Logging;
-import com.gianlu.commonutils.preferences.Prefs;
-
-import java.io.File;
 
 public class ThisApplication extends AnalyticsApplication {
 
@@ -23,21 +19,5 @@ public class ThisApplication extends AnalyticsApplication {
         Logging.clearLogs(this, 3);
 
         Aria2Ui.provider(Aria2BareConfig.class);
-
-        if (Prefs.getBoolean(PK.IS_NEW_V2, true)) {
-            File oldExec = new File(getFilesDir(), "aria2c");
-            File newExec = new File(getFilesDir(), "env/aria2c");
-            if (newExec.exists()) {
-                oldExec.deleteOnExit();
-            } else {
-                if (oldExec.exists()) {
-                    if (!oldExec.renameTo(newExec))
-                        Logging.log("Failed migrating old executable!", true);
-                }
-            }
-
-            Prefs.putString(Aria2PK.ENV_LOCATION, new File(getFilesDir(), "env").getAbsolutePath());
-            Prefs.putBoolean(PK.IS_NEW_V2, false);
-        }
     }
 }
